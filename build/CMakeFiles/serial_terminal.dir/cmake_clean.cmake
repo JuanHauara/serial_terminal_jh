05495bf39ff314991c0ad1218d535d82003643eb
JuanHauara <juanhauara@gmail.com>
2025-05-21 13:13:31 +0200

    Added build folder

---
+++ build/CMakeFiles/serial_terminal.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/serial_terminal.dir/library/Persistent_data/Persistent_data.cpp.obj"
+  "CMakeFiles/serial_terminal.dir/library/Persistent_data/Persistent_data.cpp.obj.d"
+  "CMakeFiles/serial_terminal.dir/library/SerialPortWindows/SerialPortWindows.cpp.obj"
+  "CMakeFiles/serial_terminal.dir/library/SerialPortWindows/SerialPortWindows.cpp.obj.d"
+  "CMakeFiles/serial_terminal.dir/main.cpp.obj"
+  "CMakeFiles/serial_terminal.dir/main.cpp.obj.d"
+  "libserial_terminal.dll.a"
+  "serial_terminal.exe"
+  "serial_terminal.exe.manifest"
+  "serial_terminal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/serial_terminal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
